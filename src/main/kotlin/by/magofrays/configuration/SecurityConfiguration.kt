5421ebdc68bf9d9db3122229eb0e5b487587e0c9
magofrays <vadimpozdeev2005@gmail.com>
2026-05-27 01:31:25 +0300

    http security

--- src/main/kotlin/by/magofrays/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/by/magofrays/configuration/SecurityConfiguration.kt
@@ -7,11 +7,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity
 import org.springframework.security.config.annotation.rsocket.RSocketSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
 import org.springframework.security.oauth2.server.resource.authentication.JwtReactiveAuthenticationManager
 import org.springframework.security.rsocket.core.PayloadSocketAcceptorInterceptor
+import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.CorsConfigurationSource
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
@@ -22,8 +24,8 @@ import java.security.interfaces.RSAPublicKey
 @EnableWebFluxSecurity
 @EnableRSocketSecurity
 @EnableReactiveMethodSecurity
-class SecurityConfiguration{
-    @Value($$"${spring.security.jwt.public-key}")
+class SecurityConfiguration {
+    @Value("\${spring.security.jwt.public-key}")
     lateinit var publicKey: RSAPublicKey
 
     @Bean
@@ -33,7 +35,17 @@ class SecurityConfiguration{
     }
 
     @Bean
-    fun authorization(security: RSocketSecurity): PayloadSocketAcceptorInterceptor {
+    fun securityFilterChain(security: ServerHttpSecurity): SecurityWebFilterChain {
+        return security
+            .cors { cors -> cors.configurationSource(corsConfigurationSource()) }
+            .csrf { csrf -> csrf.disable() }
+            .authorizeExchange { exchange -> exchange.anyExchange().authenticated() }
+            .oauth2ResourceServer { oauth2 -> oauth2.jwt { jwt -> jwt.jwtDecoder(reactiveJwtDecoder()) } }
+            .build()
+    }
+
+    @Bean
+    fun authorizationRSocket(security: RSocketSecurity): PayloadSocketAcceptorInterceptor {
         return security
             .authorizePayload { authorize ->
                 authorize
@@ -55,7 +67,7 @@ class SecurityConfiguration{
         cors.allowedMethods = listOf("*")
         cors.allowCredentials = true
         val source = UrlBasedCorsConfigurationSource()
-        source.apply { registerCorsConfiguration("/**", cors) }
+        source.registerCorsConfiguration("/**", cors)
         return source
     }
 
--- src/main/kotlin/by/magofrays/controller/FamilyController.kt
+++ src/main/kotlin/by/magofrays/controller/FamilyController.kt
@@ -5,6 +5,9 @@ import by.magofrays.service.NotificationService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -16,19 +19,24 @@ import java.util.*
 class FamilyController(
     private val notificationService: NotificationService,
 ) {
-    @GetMapping(path = ["notifications/connect/{memberId}"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun connectNotification(@PathVariable memberId: UUID) : Flux<NotificationDto> { // todo principal
+    @GetMapping(path = ["notifications/connect"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun connectNotification(
+        @AuthenticationPrincipal memberToken: Jwt
+    ) : Flux<NotificationDto> {
+        val memberId = UUID.fromString(memberToken.subject)
         return notificationService.connectNotifications(memberId)
     }
 
-    @GetMapping("/notifications/{memberId}")
+    @PreAuthorize("hasAuthority('USER')")
+    @GetMapping("/notifications")
     fun findAllNotificationById(
-        @PathVariable memberId: UUID, // todo principal
+        @AuthenticationPrincipal memberToken: Jwt,
         @RequestParam(defaultValue = "0") page: Int,
         @RequestParam(defaultValue = "20") size: Int,
         @RequestParam(required = false) startDate: Instant?,
         @RequestParam(required = false) endDate: Instant?
     ): Mono<Page<NotificationDto>> {
+        val memberId = UUID.fromString(memberToken.subject)
         val pageable = PageRequest.of(page, size)
         return notificationService.findAllNotificationByMember(memberId, startDate, endDate, pageable)
     }
