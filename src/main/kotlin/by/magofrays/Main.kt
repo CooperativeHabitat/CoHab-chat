2e176b4c9ff5ab6e580b4b0599b5d90603118f4f
magofrays <vadimpozdeev2005@gmail.com>
2026-06-16 12:10:10 +0300

    change application remove configuration

--- src/main/kotlin/by/magofrays/Main.kt
+++ src/main/kotlin/by/magofrays/Main.kt
@@ -1,10 +1,11 @@
 package by.magofrays
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.mongodb.autoconfigure.MongoReactiveAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
-@SpringBootApplication(exclude = [MongoReactiveAutoConfiguration::class])
+@SpringBootApplication
+@EnableReactiveMongoRepositories("by.magofrays.repository.mongo")
 class App
 
 fun main(args: Array<String>) {
